2bf396fe308bafff4a2acfab5c3d5a344c91f964
pawannagar1996 <pawannagar1996@gmail.com>
2024-03-19 19:48:19 +0530

    accordian completed

--- Js-Project/according/style.js
+++ Js-Project/according/style.js
@@ -1,19 +1,14 @@
-const clickMe = document.querySelectorAll(".accordian-item")
+const clickMe = document.querySelectorAll(".accordian-item");
 
 clickMe.forEach((item) => {
-    const showHide = document.querySelector(".accordian-context")
-    const title = item.querySelector(".accordian-title")
-
-    title.addEventListener("click", () => {
-
-        for (var i = 0; i < clickMe.length; i++) {
-            if (clickMe[i] != item) {
-                clickMe[i].classList.remove("active")
-            } else {
-                item.classList.toggle("active")
-            }
-        }
-
-    })
-
-})
+  item.addEventListener("click", () => {
+    for (var i = 0; i < clickMe.length; i++) {
+      if (clickMe[i] != item) {
+        clickMe[i].classList.remove("active");
+      } else {
+        item.classList.toggle("active");
+      
+      }
+    }
+  });
+});
