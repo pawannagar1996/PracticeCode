e628ddaf8bc9069e078bd7b47dc6890c4ed79845
pawannagar1996 <pawannagar1996@gmail.com>
2024-05-03 20:04:21 +0530

    making personal portfolio

--- ReactProject/portfolio/src/components/navbar/navbar.js
+++ ReactProject/portfolio/src/components/navbar/navbar.js
@@ -6,10 +6,18 @@ const Navbar = ()=>{
         <div className="n-wrapper">
 
             <div className="n-left">
-                <div className="n-name"></div>
+                <div className="n-logo">pawan</div>
             </div>
 
-            <div className="n-right"></div>
+            <div className="n-right">
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+
+                <button>logIn</button>
+            </div>
 
         </div>
     )
