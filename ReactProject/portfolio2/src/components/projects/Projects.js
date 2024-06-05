b3b04888b5ecdec943a9c381a47956405a6ae465
pawannagar1996 <pawannagar1996@gmail.com>
2024-06-05 18:12:08 +0530

    making project page section

--- ReactProject/portfolio2/src/components/projects/Projects.js
+++ ReactProject/portfolio2/src/components/projects/Projects.js
@@ -1,11 +1,79 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 import React from "react";
 import "./projects.css";
+
 function Projects() {
   return (
     <div className="wrapper">
       <div className="projects layout pTB100">
-        
+        <div className="main-title">
+          <h1>
+            my <span>projects</span>
+          </h1>
+          <span className="main-title-title-bg">work</span>
+        </div>
 
+        <div className="project-slider">
+        <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={false}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+        </div>
+
+        <div className='projectBtn'>
+        <button>
+              <span>All projects</span>
+              <span>i</span>
+            </button>
+        </div>
       </div>
     </div>
   );
