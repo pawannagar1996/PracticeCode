dba9b9aac3020e6e34a6b8f812155e81fcac6756
Pawan Nagar <111763436+pawannagar1996@users.noreply.github.com>
2024-07-11 20:12:22 +0530

    edit and some conditions

--- ReactProject/todo-app/src/componenets/TodoComponent.js
+++ ReactProject/todo-app/src/componenets/TodoComponent.js
@@ -1,15 +1,18 @@
 import { useRef, useState } from "react";
 
 function TodoComponent() {
-  const [todo, updateTodo] = useState([
+  const [todos, updateTodos] = useState([
     { id: "" + Date.now(), text: "default", status: "active" },
   ]);
+
+  const [addButtonDisable, updateAddButtonDisable] = useState(true)
 
   const textRef = useRef();
 
   function renderTodoAdderForm() {
     return (
       <div className="wrapper">
+
         <div className="left">
           <div>Today Tasks : 5</div>
           <div>complete : 2</div>
@@ -27,17 +30,15 @@ function TodoComponent() {
                   id="f-name"
                   placeholder="Write your next task"
                   ref={textRef}
+                  onChange={handleInputTextChange}
                 />
               </div>
-              <button onClick={(e) => addTodoList(e)}>Add</button>
+              <button onClick={(e) => addTodoList(e)}  disabled={addButtonDisable}>Add</button>
             </div>
           </form>
           {renderList()}
         </div>
-      
       </div>
-
-
     );
   }
 
@@ -48,47 +49,80 @@ function TodoComponent() {
       text: textRef.current.value,
       status: "active",
     };
-    const newTodo = [...todo];
-    newTodo.push(item);
-    updateTodo(newTodo);
+    const newTodos = [...todos];
+    newTodos.push(item);
+    updateTodos(newTodos);
     textRef.current.value = "";
-
-    console.log(todo)
+    updateAddButtonDisable(true)
+    // console.log(todos)
   }
 
-  function renderList(){
-
-    return(
-        <>
-  <ul>
-    {console.log(todo)}
-        {
-            todo.map((item)=>{
-              
-                return(
-                <li key={item.id}>
-                    <p>{item.text}</p>
-                    <div>
-                        <button>{item.status}</button>
-                        <button>Edit</button>
-                        <button>Delete</button>
-                    </div>
-                </li>
-                )
-            }
-            )
-        }
-         </ul>
-      
-        </>
-    )
+  function renderList() {
+    return (
+      <>
+        <ul>
+          {todos.map((item) => {
+            const { id, text, status } = item;
+            return (
+              <li
+                key={id}
+                id={"li--" + id}
+                className={getTodoStatusClass(status)}
+              >
+                <p>{text}</p>
+                <div>
+                  <button
+                    onClick={handleMarkDone}
+                    id={"btn-done--" + id}
+                    disabled={status === "done"}
+                  >
+                    {status}
+                  </button>
+                  <button id={"btn-edit--" + id} disabled={status === "done"}>
+                    Edit
+                  </button>
+                  <button id={"btn-delete--" + id}>Delete</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
   }
 
-  return <>
-  {renderTodoAdderForm()}
- 
- 
-  </>;
+  function handleMarkDone(event) {
+    let id = event.target.id;
+    id = id.split("--")[1];
+    const index = todos.findIndex((todo) => todo.id === id);
+    const todo = { ...todos[index], status: "done" };
+    const newTodos = [...todos];
+    newTodos[index] = todo;
+    updateTodos(newTodos);
+    console.log("index", index);
+  }
+
+  function getTodoStatusClass(status) {
+    if (status === "active") {
+      return "active";
+    } else {
+      return "done";
+    }
+  }
+
+  function handleInputTextChange(event){
+let inputText = event.target.value
+// console.log('input text has----', inputText, ' and length is', inputText.length);
+if(inputText.length > 0){
+  updateAddButtonDisable(false)
+  console.log("test")
+}
+else{  updateAddButtonDisable(true) 
+  console.log("no")
+}
+  }
+
+  return <>{renderTodoAdderForm()}</>;
 }
 
 export default TodoComponent;
