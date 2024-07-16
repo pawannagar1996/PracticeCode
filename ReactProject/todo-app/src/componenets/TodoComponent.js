44bec8adf583ba09b2d8d448622475b4d2d92eee
Pawan Nagar <111763436+pawannagar1996@users.noreply.github.com>
2024-07-16 19:38:57 +0530

    delete

--- ReactProject/todo-app/src/componenets/TodoComponent.js
+++ ReactProject/todo-app/src/componenets/TodoComponent.js
@@ -1,18 +1,15 @@
 import { useRef, useState } from "react";
 
 function TodoComponent() {
-  const [todos, updateTodos] = useState([
-    { id: "" + Date.now(), text: "default", status: "active",editing :true},
-  ]);
+  const [todos, updateTodos] = useState([]);
 
-  const [addButtonDisable, updateAddButtonDisable] = useState(true)
+  const [addButtonDisable, updateAddButtonDisable] = useState(true);
 
   const textRef = useRef();
 
   function renderTodoAdderForm() {
     return (
       <div className="wrapper">
-
         <div className="left">
           <div>Today Tasks : 5</div>
           <div>complete : 2</div>
@@ -33,7 +30,12 @@ function TodoComponent() {
                   onChange={handleInputTextChange}
                 />
               </div>
-              <button onClick={(e) => addTodoList(e)}  disabled={addButtonDisable}>Add</button>
+              <button
+                onClick={(e) => addTodoList(e)}
+                disabled={addButtonDisable}
+              >
+                Add
+              </button>
             </div>
           </form>
           {renderList()}
@@ -48,13 +50,13 @@ function TodoComponent() {
       id: "" + Date.now(),
       text: textRef.current.value,
       status: "active",
-      editing:false
+      editing: false,
     };
     const newTodos = [...todos];
     newTodos.push(item);
     updateTodos(newTodos);
     textRef.current.value = "";
-    updateAddButtonDisable(true)
+    updateAddButtonDisable(true);
 
     // console.log(todos)
   }
@@ -64,25 +66,30 @@ function TodoComponent() {
       <>
         <ul>
           {todos.map((item) => {
-            const { id, text, status ,editing } = item;
+            const { id, text, status, editing } = item;
             return (
-
               <li
                 key={id}
                 id={"li--" + id}
                 className={getTodoStatusClass(status)}
               >
-                   {editing ? (<input defaultValue={text}  id={'input-todo--'+id} />) :(<span>{text}</span>)}
-              
+                {editing ? (
+                  <input defaultValue={text} id={"input-todo--" + id} />
+                ) : (
+                  <span>{text}</span>
+                )}
+
                 <div>
-                {editing ? null : 
-                 ( <button
-                  id={'btn-done--'+id}
-                  onClick={handleMarkDone} 
-                  disabled={status === 'done'}>
-                   {status}
-                </button>)}
-                {/* {editing !== true  && (
+                  {editing ? null : (
+                    <button
+                      id={"btn-done--" + id}
+                      onClick={handleMarkDone}
+                      disabled={status === "done"}
+                    >
+                      {status}
+                    </button>
+                  )}
+                  {/* {editing !== true  && (
                   <button
                     id={'btn-done--'+id}
                     onClick={handleMarkDone} 
@@ -90,13 +97,16 @@ function TodoComponent() {
                      {status}
                   </button>
                 )} */}
-                  <button id={"btn-edit--" + id}
-                  onClick={handleEdit}
-                   disabled={status === "done"}>
+                  <button
+                    id={"btn-edit--" + id}
+                    onClick={handleEdit}
+                    disabled={status === "done"}
+                  >
                     {editing ? "edit done" : "edit"}
-                    
                   </button>
-                  <button id={"btn-delete--" + id}>Delete</button>
+                  <button id={`${id}-delete`} onClick={handleDelete}>
+                    Delete
+                  </button>
                 </div>
               </li>
             );
@@ -114,7 +124,7 @@ function TodoComponent() {
     const newTodos = [...todos];
     newTodos[index] = todo;
     updateTodos(newTodos);
-    console.log("index", index);
+    // console.log("index", index);
   }
 
   function getTodoStatusClass(status) {
@@ -125,31 +135,28 @@ function TodoComponent() {
     }
   }
 
-  function handleInputTextChange(event){
-let inputText = event.target.value
-// console.log('input text has----', inputText, ' and length is', inputText.length);
-if(inputText.length > 0){
-  updateAddButtonDisable(false)
-  console.log("test")
-}
-else{  updateAddButtonDisable(true) 
-  console.log("no")
-}
+  function handleInputTextChange(event) {
+    let inputText = event.target.value;
+    // console.log('input text has----', inputText, ' and length is', inputText.length);
+    if (inputText.length > 0) {
+      updateAddButtonDisable(false);
+      console.log("test");
+    } else {
+      updateAddButtonDisable(true);
+      console.log("no");
+    }
   }
 
-  function handleEdit(event){
+  function handleEdit(event) {
     let id = event.target.id;
     id = id.split("--")[1];
     const index = todos.findIndex((todo) => todo.id === id);
-    const todo = {...todos[index]};
-  
-
+    const todo = { ...todos[index] };
     // console.log('todo.editing ', todo.editing);
-
-    if(todo.editing) { 
-      const inputTodoEle = document.getElementById('input-todo--'+id)
-      console.log('inputTodoEle ', inputTodoEle);
-      console.log('inputTodoEle value', inputTodoEle.value);
+    if (todo.editing) {
+      const inputTodoEle = document.getElementById("input-todo--" + id);
+      console.log("inputTodoEle ", inputTodoEle);
+      console.log("inputTodoEle value", inputTodoEle.value);
       todo.text = inputTodoEle.value;
     }
 
@@ -158,6 +165,14 @@ else{  updateAddButtonDisable(true)
     const newTodos = [...todos];
     newTodos[index] = todo;
     updateTodos(newTodos);
+  }
+
+  function handleDelete(event) {
+    const id = event.target.id.split("-")[0];
+    console.log(id);
+
+    const cloneList = todos.filter((item) => item.id !== id);
+    updateTodos(cloneList);
   }
 
   return <>{renderTodoAdderForm()}</>;
